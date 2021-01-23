bbc05f9d93e59b0b9344d25adcf2f78001599a9a
Parshva Doshi <parshva101@gmail.com>
2021-01-24 00:52:02 +0530

    init: cameraApp

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:async';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Camera App',
+        ),
+      ),
+      body: Center(
+        child: _image == null ? Text(' No Image.') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _optionsDialogBox,
+        child: Icon(Icons.add_a_photo),
+        tooltip: 'Open Camera',
+      ),
+    );
+  }
+
+  Future<void> _optionsDialogBox() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: Text(
+                      'Take A Picture',
+                    ),
+                    onTap: openCamera,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      'Select Image From Gallery',
+                    ),
+                    onTap: openGallery,
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future openCamera() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future openGallery() async {
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = picture;
+    });
+  }
+}
